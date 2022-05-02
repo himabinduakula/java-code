c25e1eb8d342f1c4583c7564d0844f6d6c47e89b
himabinduakula <akulahima09@gmail.com>
2022-05-02 10:55:47 +0000

    commit 2:added comment lines and README.md file

---
+++ README.md
@@ -0,0 +1,1 @@
+this code is sample code for devops sesssion written by :Himabinduakula
--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,5 @@
 // First java hello world program
+// this program will print Helloworld!
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
