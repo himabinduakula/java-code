b3251e8afd991ed81ce94e4c9e8ee496a051a721
root <root@ip-172-31-26-238.ec2.internal>
2022-05-04 15:39:14 +0000

    added one more print statement

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,7 @@
 // this program will print Helloworld!
 public class HelloWorld {
     public static void main(String[] args) {
+System.out.println("Hello, World!");
         System.out.println("Hello, World!");
 	// prints welcome message
 	System.out.println("Hello, Welcome to git");
